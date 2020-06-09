fb81746c51234b7db08fe3cf516b79ad893b2701
dhirahdris <dhirahdris@gmail.com>
2020-06-09 22:01:45 +0800

    connection

---
+++ connection.php
@@ -0,0 +1,12 @@
+<?php
+
+$ser="den1.mysql5.gear.host";
+$user="mmdb";
+$pass="Multimedia@1";
+$db="mmdb";
+
+$con=mysqli_connect($ser, $user, $pass, $db) or die ("Connection Failed");
+
+//echo "Connection Success";
+
+?>
