68016e7c8eafa46a96c1ae923966061ceaed4968
dhirahdris <66379269+dhirahdris@users.noreply.github.com>
2020-06-10 23:56:46 +0800

    change projdesc

--- projdesc.php
+++ projdesc.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html lang="zxx">
 <head>
@@ -69,7 +68,8 @@
 			<div class="copyright">BITP3353 GROUP 9  @ All rights reserved</div>
 		</header>
 		<!-- header section end -->
-<!-- Page section -->
+
+		<!-- Page section -->
 		<div class="page-section home-page">
 			<div class="hero-slider owl-carousel">
 				<div class="slider-item d-flex align-items-center set-bg" data-setbg="img/ftmk.jpg" data-hash="slide-1">
@@ -82,7 +82,7 @@
 					</div>
 				
 				</div>
-			
+				
 		<!-- Page section end-->
 	</div>
 
@@ -110,116 +110,3 @@
 
 	</body>
 </html>
-=======
-<!DOCTYPE html>
-<html lang="zxx">
-<head>
-	<title>FTMK NAVIGATION</title>
-	<meta charset="UTF-8">
-	<meta name="description" content="Photo Gallery HTML Template">
-	<meta name="keywords" content="endGam,gGaming, magazine, html">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<!-- Favicon -->
-	<link href="img/Logo-FTMK.png" rel="shortcut icon"/>
-
-	<!-- Google Font -->
-	<link href="https://fonts.googleapis.com/css?family=PT+Sans:400,400i,700,700i" rel="stylesheet">
-
-
-	<!-- Stylesheets -->
-	<link rel="stylesheet" href="css/bootstrap.min.css"/>
-	<link rel="stylesheet" href="css/font-awesome.min.css"/>
-	<link rel="stylesheet" href="css/owl.carousel.min.css"/>
-	<link rel="stylesheet" href="css/animate.css"/>
-
-	<!-- Main Stylesheets -->
-	<link rel="stylesheet" href="css/style.css"/>
-
-
-	<!--[if lt IE 9]>
-		  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-	<![endif]-->
-
-</head>
-<body>
-	<!-- Page Preloder -->
-	<div id="preloder">
-		<div class="loader"></div>
-	</div>
-
-	<!-- Top right elements -->
-	<div class="spacial-controls">
-		<div class="search-switch"><img src="img/search-icon.png" alt=""></div>
-		<div class="nav-switch-warp">
-			<div class="nav-switch">
-				<div class="ns-bar"></div>
-			</div>
-		</div>
-	</div>
-	<!-- Top right elements end -->
-
-	<div class="main-warp">
-		<!-- header section -->
-		<header class="header-section">
-			<div class="header-close">
-				<i class="fa fa-times"></i>
-			</div>
-			<div class="header-warp">
-				<a href="" class="site-logo">
-					<img src="./img/Logo-FTMK.png" alt="">
-				</a>
-				<img src="img/menu-icon.png" alt="" class="menu-icon">
-				<ul class="main-menu">
-					<li class="active"><a href="./home.php">Home</a></li>
-					<li><a href="./about.php">About Us</a></li>
-					<li class="active"><a href="./contact.php">Contact & Location</a></li>
-				</ul>
-
-			<div class="copyright">BITP3353 GROUP 9  @ All rights reserved</div>
-		</header>
-		<!-- header section end -->
-<!-- Page section -->
-		<div class="page-section home-page">
-			<div class="hero-slider owl-carousel">
-				<div class="slider-item d-flex align-items-center set-bg" data-setbg="img/ftmk.jpg" data-hash="slide-1">
-					<div class="si-text-box">
-						<span>PROJECT DESCRIPTION</span>
-						<p>WELCOME TO FTMK NAVIAPP!!
-						<br />
-						This app is solely developed for newcomers or visitors to find your way around our bulding. Just type in the location that you need to be at and this app will show you the way to that location in the building. We even have voice overs that can guideyou through your way.
-						</p>
-					</div>
-				
-				</div>
-				
-			<a href="img/chart.pdf" class="site-btn about-btn">CLICK ME TO SEE THE ORGANISATION CHART</a>
-					
-		<!-- Page section end-->
-	</div>
-
-	<!-- Search model -->
-	<div class="search-model">
-		<div class="h-100 d-flex align-items-center justify-content-center">
-			<div class="search-close-switch">x</div>
-			<form class="search-moderl-form">
-				<input type="text" id="search-input" placeholder="Search here.....">
-			</form>
-		</div>
-	</div>
-	<!-- Search model end -->
-
-
-	<!--====== Javascripts & Jquery ======-->
-	<script src="js/jquery-3.2.1.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-	<script src="js/owl.carousel.min.js"></script>
-	<script src="js/jquery.nicescroll.min.js"></script>
-	<script src="js/isotope.pkgd.min.js"></script>
-	<script src="js/imagesloaded.pkgd.min.js"></script>
-	<script src="js/circle-progress.min.js"></script>
-	<script src="js/main.js"></script>
-
-	</body>
-</html>
->>>>>>> 2d8148fd4a9698bb7da4947cebe076e1cb19a699
