ac1f0ce7b399daaefe771679b56900699e93bc47
dhirahdris <66379269+dhirahdris@users.noreply.github.com>
2020-06-11 00:17:30 +0800

    chart directory

--- index.php
+++ index.php
@@ -62,7 +62,7 @@
 					<li><a href="./about.php">About Us</a></li>
 					<li><a href="./contact.php">Contact & Location</a></li>
 					<li><a href="./projdesc.php">Project Description</a></li>
-					<li><a href=".img/chart.pdf">Organisation Chart</a></li>
+					<li><a href="./img/chart.pdf">Organisation Chart</a></li>
 					
 				</ul>
 
